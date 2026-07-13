a73239da9f27ce72f4e5d996f2989646ef5dc965
林俊遑 <linjack9001155@gmail.com>
2026-07-13 21:30:56 +0800

    add Standalone App Mode

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
 import path from "path";
 
+const isVercel = process.env.VERCEL === "1";
+
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root: path.resolve(typeof __dirname !== "undefined" ? __dirname : "."),
-  },
+  ...(isVercel
+    ? {}
+    : {
+        turbopack: {
+          root: path.resolve(typeof __dirname !== "undefined" ? __dirname : "."),
+        },
+      }),
 
   allowedDevOrigins: ["192.168.0.181", "192.168.0.181:3000"],
-
 };
 export default nextConfig;
